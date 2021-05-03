0318bd56b2ac0a7cfe488bf33c28deb8e2fbff13
mengxiangni <1666878496@qq.com>
2021-05-03 16:59:50 +0800

    线下收款界面（加入购物车界面（MerSellFrame），支付界面（MerPayFrame），二位码支付界面（MerCodePayFrame），Pos机支付界面（MerPosCodePayFrame））但是往数据库插入订单没搞

---
+++ src/main/java/swing/MerPayFrame.java
@@ -0,0 +1,131 @@
+/*
+ * Created by JFormDesigner on Mon May 03 15:09:06 CST 2021
+ */
+
+package swing;
+
+import util.ShowQRCode;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
+/**
+ * 商家结账页面，可选择三种付款方式
+ */
+public class MerPayFrame extends JFrame {
+    public static void main(String[] args) {
+    }
+    public MerPayFrame(int sumprice) {
+        initComponents(sumprice);
+    }
+
+    private void initComponents(final int sumprice) {
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+        label1 = new JLabel();
+        textField1 = new JTextField();
+        label2 = new JLabel();
+        textField2 = new JTextField();
+        label3 = new JLabel();
+        textField3 = new JTextField();
+        button1 = new JButton();
+        button2 = new JButton();
+        button3=new JButton();
+        label1.setText("\u603b\u989d\uff1a");
+        contentPane.add(label1);
+        label1.setBounds(new Rectangle(new Point(80, 100), label1.getPreferredSize()));
+        contentPane.add(textField1);
+        textField1.setBounds(145, 95, 155, textField1.getPreferredSize().height);
+        //---- label2 ----
+        label2.setText("\u652f\u4ed8\uff1a");
+        contentPane.add(label2);
+        label2.setBounds(new Rectangle(new Point(80, 135), label2.getPreferredSize()));
+        contentPane.add(textField2);
+        textField2.setBounds(145, 135, 155, textField2.getPreferredSize().height);
+        //---- label3 ----
+        label3.setText("\u627e\u96f6\uff1a");
+        contentPane.add(label3);
+        label3.setBounds(new Rectangle(new Point(80, 175), label3.getPreferredSize()));
+        contentPane.add(textField3);
+        textField3.setBounds(145, 170, 155, textField3.getPreferredSize().height);
+
+        //---- button1 ----
+        button1.setText("\u73b0\u91d1\u652f\u4ed8");
+        contentPane.add(button1);
+        button1.setBounds(new Rectangle(new Point(60, 215), button1.getPreferredSize()));
+        button1.setBackground(Color.BLUE);
+
+        //---- button2 ----
+        button2.setText("\u4ed8\u6b3e\u7801\u652f\u4ed8");
+        contentPane.add(button2);
+        button2.setBounds(new Rectangle(new Point(165, 215), button2.getPreferredSize()));
+        button2.setBackground(Color.RED);
+
+        //----button3----
+        button3.setText("Pos\u673a\u4ed8\u6b3e");
+        contentPane.add(button3);
+        button3.setBounds(new Rectangle(new Point(280, 215), button3.getPreferredSize()));
+        button3.setBackground(Color.ORANGE);
+        //显示总额
+        textField1.setText(""+sumprice);
+        textField2.setText(""+sumprice);
+        textField3.setText("0");
+        //现金支付
+        button1.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        textField1.setText("");
+                        textField2.setText("");
+                        textField3.setText("");
+                    }
+                }
+        );
+        //二维码支付
+        button2.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        util.Main main = new util.Main();
+                        main.test_trade_precreate(Integer.parseInt(textField1.getText()));
+                        List<File> list = new ArrayList<File>();
+                        list= ShowQRCode.getFileSort("E:\\");
+                        String endfileurl=list.get(list.size()-1).getAbsolutePath();
+                        MerCodePayFrame codePayFrame=new MerCodePayFrame(endfileurl);
+                    }
+                }
+        );
+        //pos机支付
+        button3.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        MerPosCodePayFrame merPosCodePayFrame=new MerPosCodePayFrame(sumprice);
+
+                    }
+                }
+        );
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        setVisible(true);
+    }
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+}
---
+++ src/main/java/swing/OrderFrame.java
@@ -0,0 +1,99 @@
+package swing;
+
+import entity.MilkTeaBean;
+import util.ShowQRCode;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Sun May 02 15:45:20 CST 2021
+ */
+
+
+
+/**
+ * @author 1
+ */
+public class OrderFrame extends JFrame {
+    public OrderFrame(List<MilkTeaBean>list) {
+        initComponents(list);
+    }
+
+    private void initComponents(List<MilkTeaBean>list) {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        label1 = new JLabel();
+        label2=new JLabel();
+        textField1 = new JTextField();
+        textField2=new JTextField();
+        button1 = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //---- label1 ----
+        label1.setText("\u603b\u91d1\u989d\uff1a");
+        contentPane.add(label1);
+        label1.setBounds(new Rectangle(new Point(40, 95), label1.getPreferredSize()));
+        contentPane.add(textField1);
+        textField1.setBounds(105, 95, 140, textField1.getPreferredSize().height);
+        int sumpirce=0;
+        for(int i=0;i<list.size();i++){
+            sumpirce=sumpirce+list.get(i).getNumber()*Integer.parseInt(list.get(i).getSellprice());
+        }
+        textField1.setText(""+sumpirce);
+        textField1.setEnabled(false);
+        //---label2---//
+        label2.setText("\u914d\u9001\u65f6\u95f4\uff1a");
+        contentPane.add(label2);
+        label2.setBounds(new Rectangle(new Point(40, 125), label2.getPreferredSize()));
+        contentPane.add(textField2);
+        textField2.setBounds(105, 125, 140, textField2.getPreferredSize().height);
+        Date date=new Date();
+        Date now_time=new Date(date.getTime()+600000);
+        SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        String time=format.format(now_time);
+        textField2.setText(time);
+        textField2.setForeground(new Color(204,204,204));
+
+        //---- button1 ----
+        button1.setText("\u7ed3\u8d26");
+        contentPane.add(button1);
+        button1.setBounds(new Rectangle(new Point(230, 165), button1.getPreferredSize()));
+        //---支付---//
+        button1.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        util.Main main = new util.Main();
+                        main.test_trade_precreate(Integer.parseInt(textField1.getText()));
+                        List<File> list = new ArrayList<File>();
+                        list= ShowQRCode.getFileSort("E:\\");
+                        String endfileurl=list.get(list.size()-1).getAbsolutePath();
+                        PayFrame payFrame=new PayFrame(endfileurl);
+                    }
+                }
+        );
+
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+        setVisible(true);
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    private JLabel label1;
+    private JLabel label2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
