c1266fbc56670ee27a4ee1b283b59cf9d99970c6
Pazzza <3497034242@qq.com>
2021-05-05 15:30:50 +0800

    优化代码

--- src/main/java/swing/Underway/underway.java
+++ src/main/java/swing/Underway/underway.java
@@ -8,8 +8,6 @@ import java.util.Date;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -37,19 +35,12 @@ public class underway extends JFrame {
         button3 = new JButton();
         button4 = new JButton();
         button5 = new JButton();
-        DefaultTableModel tableModel = new DefaultTableModel(queryData1(), head) {
+        DefaultTableModel tableMode = new DefaultTableModel(queryData(), head) {
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
         };
-        table1.setModel(tableModel);
-        DefaultTableModel tableMode2 = new DefaultTableModel(queryData2(), head) {
-            public boolean isCellEditable(int row, int column) {
-                return false;
-            }
-        };
-        table2.setModel(tableMode2);
-
+        table1.setModel(tableMode);
         //======== this ========
         Container contentPane = getContentPane();
         contentPane.setLayout(null);
@@ -69,12 +60,12 @@ public class underway extends JFrame {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        DefaultTableModel tableModel = new DefaultTableModel(queryData1(), head) {
+                        DefaultTableModel tableMode = new DefaultTableModel(queryData(), head) {
                             public boolean isCellEditable(int row, int column) {
                                 return false;
                             }
                         };
-                        table1.setModel(tableModel);
+                        table1.setModel(tableMode);
                     }
                 }
         );
@@ -113,11 +104,11 @@ public class underway extends JFrame {
                 }
         );
 
+
         //======== scrollPane2 ========
         {
             scrollPane2.setViewportView(table2);
         }
-        scrollPane2.setVisible(false);
         contentPane.add(scrollPane2);
         scrollPane2.setBounds(60, 60, 452, 213);
 
@@ -130,20 +121,22 @@ public class underway extends JFrame {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        DefaultTableModel tableMode2 = new DefaultTableModel(queryData2(), head) {
+                        DefaultTableModel tableMode = new DefaultTableModel(queryData(), head) {
                             public boolean isCellEditable(int row, int column) {
                                 return false;
                             }
                         };
-                        table2.setModel(tableMode2);
+                        table2.setModel(tableMode);
                     }
                 }
         );
+
 
         //---- button4 ----
         button4.setText("\u6d3e\u9001\u8ba2\u5355");
         button4.setFont(new Font("\u5b8b\u4f53", Font.PLAIN, 22));
         contentPane.add(button4);
+        button4.setBounds(125, 10, 150, button4.getPreferredSize().height);
         button4.setBounds(125, 10, 150, button4.getPreferredSize().height);
         button4.addActionListener(
                 new ActionListener() {
@@ -154,6 +147,7 @@ public class underway extends JFrame {
                         button3.setVisible(false);
                         scrollPane2.setVisible(false);
                         scrollPane1.setVisible(true);
+                        num = 1;
                     }
                 }
         );
@@ -172,6 +166,7 @@ public class underway extends JFrame {
                         button3.setVisible(true);
                         scrollPane2.setVisible(true);
                         scrollPane1.setVisible(false);
+                        num = 0;
                     }
                 }
         );
@@ -183,7 +178,7 @@ public class underway extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
-    public Object[][] queryData1() {
+    public Object[][] queryData() {
         java.util.List<Comorder> list = new ArrayList<Comorder>();
             Dbutil dbutil = new Dbutil();
             String sql = "SELECT *\n" +
@@ -191,11 +186,13 @@ public class underway extends JFrame {
                     "FROM comorder c,users u\n" +
                     "WHERE c.username=u.username)\n" +
                     "WHERE uname=? and status=?";
-            ResultSet rs = null;
+
+        ResultSet rs = null;
             PreparedStatement ptem = dbutil.getPs(sql);
         try {
             ptem.setString(1,"黄志国");
-            ptem.setString(2,"1");
+            ptem.setString(2, String.valueOf(num));
+            System.out.println(sql);
             rs=ptem.executeQuery();
             while (rs.next()) {//每循环一次给用户赋值
                 //将数据库中用户的信息存放在集合中
@@ -224,47 +221,7 @@ public class underway extends JFrame {
         }
         return data;
     }
-    public Object[][] queryData2() {
-        java.util.List<Comorder> list = new ArrayList<Comorder>();
-        Dbutil dbutil = new Dbutil();
-        String sql = "SELECT *\n" +
-                "FROM(SELECT c.ordid,u.uname,c.ordertime,c.trantime,c.receivetime,c.status\n" +
-                "FROM comorder c,users u\n" +
-                "WHERE c.username=u.username)\n" +
-                "WHERE uname=? and status=?";
-        ResultSet rs = null;
-        PreparedStatement ptem = dbutil.getPs(sql);
-        try {
-            ptem.setString(1,"黄志国");
-            ptem.setString(2,"0");
-            rs=ptem.executeQuery();
-            while (rs.next()) {//每循环一次给用户赋值
-                //将数据库中用户的信息存放在集合中
-                Comorder comorder = new Comorder();
-                comorder.setOrdid(rs.getString("ordid"));
-                comorder.setUsername(rs.getString("uname"));
-                comorder.setOrdertime(rs.getString("ordertime"));
-                comorder.setTrantime(rs.getString("trantime"));
-                comorder.setReceivetime((rs.getString("receivetime")));
-                comorder.setStatus(rs.getString("status"));
-                list.add(comorder);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        data = new Object[list.size()][head.length];
-        for (int i = 0; i < list.size(); i++) {//将集合中的信息放入二维数组中
-            for (int j = 0; j < head.length; j++) {
-                data[i][0] = list.get(i).getOrdid();
-                data[i][1] = list.get(i).getUsername();
-                data[i][2] = list.get(i).getOrdertime();
-                data[i][3] = list.get(i).getTrantime();
-                data[i][4] = list.get(i).getReceivetime();
-                data[i][5] = list.get(i).getStatus();
-            }
-        }
-        return data;
-    }
+
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     private JScrollPane scrollPane1;
@@ -278,5 +235,6 @@ public class underway extends JFrame {
     private JButton button5;
     private Object[][] data = null;
     private String head[] = {"订单编号", "用户名", "下单时间","配送时间","到达时间","订单状态"};
+    private int num = 1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
